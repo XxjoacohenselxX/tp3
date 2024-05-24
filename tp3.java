4300b25e7cf6b5839113ea2bae3ed8730a5ad7fa
XxjoacohenselxX <135734983+XxjoacohenselxX@users.noreply.github.com>
2024-05-24 14:06:53 -0300

    Update tp3.java

--- tp3.java
+++ tp3.java
@@ -8,7 +8,7 @@ class tp3
         Vector<String> alumnos= new Vector<>();
 
         //TODO Agrega una línea con tu nombre y apellido
-        alumnos.add("Nombre Apellido");
+        alumnos.add("Joaquin Hensel");
 
         System.out.println("Alumnos " + alumnos);        
     }
